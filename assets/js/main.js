20e8fced7c22e28ab42f367f0bccb1f2498b5ecd
HossamEisa <39234961+HossamEisa@users.noreply.github.com>
2023-06-18 16:16:47 +0300

    add scrolling header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,30 @@
+var prevScrollpos = window.scrollY;
+
+/* Get the header element and it's position */
+var headerDiv = document.getElementById("header");
+var headerBottom = headerDiv.offsetTop + headerDiv.offsetHeight;
+
+window.onscroll = function () {
+  var currentScrollPos = window.scrollY;
+
+  /* if scrolling down, let it scroll out of view as normal */
+  if (prevScrollpos <= currentScrollPos) {
+    headerDiv.classList.remove("fixedToTop");
+    headerDiv.classList.remove("fixed");
+    headerDiv.style.top = "-7.2rem";
+  }
+  else if (prevScrollpos > 100) {
+    headerDiv.classList.add("fixed");
+  } else {
+    /* otherwise if we're scrolling up, fix the nav to the top */
+    headerDiv.classList.add("fixedToTop");
+    headerDiv.classList.add("fixed");
+    headerDiv.style.top = "0";
+  }
+
+  prevScrollpos = currentScrollPos;
+};
+
 // Init Latest News Carosuel
 var latestNewsCarousel = new Swiper(".latest-news-carousel ", {
   effect: "cube",
